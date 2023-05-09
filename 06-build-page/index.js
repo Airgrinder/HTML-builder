ae5ee58e887e5d86ca46e8d03595b7a25aabb48a
Airgrinder <51jework@gmail.com>
2023-05-09 05:25:07 +0300

    fix: fix bug with double footer 06 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,12 +5,13 @@ const p = (__dirname);
 const style = p + '/project-dist/style.css';
 const template = p + '/project-dist/index.html';
 
-
 function createDir(dir) {
     fs.stat(dir, function (err) {
         if (!err) {
+            return;
         } else if (err.code === 'ENOENT') {
             fs.mkdir(dir, err => {
+                if (err) throw err;
             });
         }
     });
@@ -21,10 +22,11 @@ createDir(p + '/project-dist/assets');
 
 try {
     fs.readdir(p + '/assets', (err, dirs) => {
+        if (err) throw err;
         for (const dir of dirs) {
             createDir(p + '/project-dist/assets/' + dir);
             fs.readdir(p + '/assets/' + dir, (err, files) => {
-                if (err) ;
+                if (err) throw err;
                 for (const file of files) {
                     fs.copyFile(p + '/assets/' + dir + '/' + file, p + '/project-dist/assets/' + dir + '/' + file, err => {
                         if (err) throw err;
@@ -38,10 +40,8 @@ try {
 }
 
 function createFile(file) {
-    fs.writeFile(file, '', () => {
-    });
+    fs.writeFile(file, '', () => {});
 }
-
 
 function checkFile(file) {
     fs.stat(file, function (err) {
@@ -60,31 +60,31 @@ checkFile(style);
 
 async function buildPage() {
     try {
-        await fs.readdir(p + '/styles', (err, files) => {
-            for (const file of files) {
-                if (path.extname(file) === '.css') {
-                    let readStream = fs.ReadStream(p + '/styles/' + file, 'utf-8');
-                    readStream.on('data', text => {
-                        fs.writeFile(p + '/project-dist/style.css', '', (err) => {
-                            if (err) console.log(err);
-                        });
-                        fs.appendFile(p + '/project-dist/style.css', text, err => {
-                            if (err) {
-                                throw err;
-                            }
-                        });
+        const files = await fs.promises.readdir(p + '/styles');
+        for (const file of files) {
+            if (path.extname(file) === '.css') {
+                let readStream = fs.ReadStream(p + '/styles/' + file, 'utf-8');
+                readStream.on('data', text => {
+                    fs.writeFile(p + '/project-dist/style.css', '', (err) => {
+                        if (err) console.log(err);
                     });
-                }
+                    fs.appendFile(p + '/project-dist/style.css', text, err => {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                });
             }
-        });
+        }
     } catch (err) {
         console.error(err);
     }
 
     try {
         const readStream = fs.ReadStream(p + '/template.html', 'utf-8');
-        await readStream.on('data', text => {
+        readStream.on('data', text => {
             fs.readdir(p + '/components', (err, files) => {
+                if (err) throw err;
                 for (const file of files) {
                     const readStream = fs.ReadStream(p + '/components/' + file, 'utf-8');
                     readStream.on('data', html => {
@@ -100,8 +100,6 @@ async function buildPage() {
     } catch (err) {
         console.error(err);
     }
-
 }
 
-buildPage();
 buildPage();
